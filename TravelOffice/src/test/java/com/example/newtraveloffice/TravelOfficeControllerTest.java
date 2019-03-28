3af2f745b866021a3741fa956fb420341d5541b9
natcka <n.nawrocka@gmail.com>
2019-03-28 15:35:37 +0100

    fourth

--- TravelOffice/src/test/java/com/example/newtraveloffice/TravelOfficeControllerTest.java
+++ TravelOffice/src/test/java/com/example/newtraveloffice/TravelOfficeControllerTest.java
@@ -9,8 +9,10 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,22 +31,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class TravelOfficeControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private TravelOfficeService travelOfficeService;
-
-    @InjectMocks
-    private TravelOfficeController travelOfficeController;
-
-    @Before
-    public void init(){
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(travelOfficeController)
-      //          .addFilters(new CorsFilter())
-                .build();
-    }
+//
+//    @InjectMocks
+//    private TravelOfficeController travelOfficeController;
+//
+//    @Before
+//    public void init(){
+//        MockitoAnnotations.initMocks(this);
+//        mockMvc = MockMvcBuilders
+//                .standaloneSetup(travelOfficeController)
+//      //          .addFilters(new CorsFilter())
+//                .build();
+//    }
 
     @Test
     public void addCustomer() throws Exception {
@@ -124,7 +127,7 @@ public class TravelOfficeControllerTest {
         verify(travelOfficeService, times(1)).assign(customer.getSurname(), abroadTrip.getDestination());
         verifyNoMoreInteractions(travelOfficeService);
     }
-
+//
 //    @Test
 //    public void addTrip() throws Exception {
 //        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
